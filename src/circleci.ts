ca5bc4bbfdf2ab1c4ecb57daaabd02c615d11ee7
Jordon de Hoog <jordon.dehoog@gmail.com>
2018-07-19 18:30:06 -0400

    build(package.json): add dev scripts

--- src/circleci.ts
+++ src/circleci.ts
@@ -13,7 +13,8 @@ import {
   ArtifactResponse,
   FollowProjectResponse,
   Me,
-  BuildSummaryResponse
+  BuildSummaryResponse,
+  FetchBuildResponse
 } from "./types";
 
 export const API_BASE = "https://circleci.com/api/v1.1";
@@ -22,7 +23,7 @@ export const API_PROJECT = `${API_BASE}/project`;
 export const API_ALL_PROJECTS = `${API_BASE}/projects`;
 export const API_RECENT_BUILDS = `${API_BASE}/recent-builds`;
 
-export function createVcsUrl({ type, owner, repo }: GitInfo) {
+function createVcsUrl({ type = GitType.GITHUB, owner, repo }: GitInfo) {
   return `${API_PROJECT}/${type}/${owner}/${repo}`;
 }
 
@@ -30,8 +31,8 @@ export function createVcsUrl({ type, owner, repo }: GitInfo) {
 
 /**
  * Authenticated user info
- * GET - /me
  * @see https://circleci.com/docs/api/v1-reference/#getting-started
+ * @example GET - /me
  */
 export function getMe(token: string): Promise<MeResponse> {
   return client(token).get<MeResponse>(API_ME);
@@ -39,21 +40,89 @@ export function getMe(token: string): Promise<MeResponse> {
 
 /**
  * All followed projects:
- * GET - /projects
  * @see https://circleci.com/docs/api/v1-reference/#projects
+ * @example GET - /projects
  */
 export function getAllProjects(token: string): Promise<AllProjectsResponse> {
   return client(token).get<AllProjectsResponse>(API_ALL_PROJECTS);
 }
 
-export function getRecentBuilds(token: string): Promise<BuildSummaryResponse> {
-  return client(token).get<BuildSummaryResponse>(API_RECENT_BUILDS);
+/**
+ * Get all recent builds for CircleCI user
+ *
+ * @see https://circleci.com/docs/api/v1-reference/#recent-builds
+ * @example GET - /recent-builds?limit=10&offset=5
+ *
+ * @param token - CircleCI API token
+ * @param limit - optional - Limit the number of builds returned, max=100
+ * @param offset - optional -builds starting from this offset
+ * @returns List of recent build summaries
+ */
+export function getRecentBuilds(
+  token: string,
+  { limit, offset }: Options = {}
+): Promise<BuildSummaryResponse> {
+  const url = `${API_RECENT_BUILDS}${queryParams({ limit, offset })}`;
+  return client(token).get<BuildSummaryResponse>(url);
 }
 
 /**
- * Latest build artifacts:
- * GET - /project/:vcs-type/:username/:project/latest/artifacts?branch=:branch&filter=:filter
+ * Get recent build summaries for a project
+ *
+ * Supported query parameters:
+ *
+ * @example limit - The number of builds to return. Maximum 100, defaults to 30.
+ * @example offset - builds starting from this offset, defaults to 0.
+ * @example filter -Restricts which builds are returned. Set to "completed", "successful", "failed", "running"
+ *
+ * @see https://circleci.com/docs/api/v1-reference/#recent-builds-project
+ * @example GET - /project/:vcs-type/:username/:project?circle-token=:token&limit=20&offset=5&filter=completed
+ *
+ * Get recent builds for a project and branch
+ * @see https://circleci.com/docs/api/v1-reference/#recent-builds-project-branch
+ * @example GET - /project/:vcs-type/:username/:project/tree/:branch
+ *
+ * @see FullRequest
+ * @param token - CircleCI API token
+ * @param vcs - Get builds for this project
+ * @param branch - Optional - Get builds for single branch
+ * @param opts - Optional - Query parameters
+ * @returns A list of build summaries
+ */
+export function getBuildSummaries({
+  token,
+  vcs,
+  options: { branch = "", ...opts } = {}
+}: FullRequest): Promise<BuildSummaryResponse> {
+  const url = `${createVcsUrl(vcs)}${branch ? `/tree/${branch}` : ""}`;
+  const params = queryParams(opts, true);
+  return client(token).get<BuildSummaryResponse>(`${url}${params}`);
+}
+
+/**
+ * Get full build details for a single build
+ *
+ * @see https://circleci.com/docs/api/v1-reference/#build
+ * @example /project/:vcs-type/:username/:project/:build_num
+ *
+ * @param token - CircleCI API token
+ * @param buildNumber - Target build number
+ * @returns Full build details including build steps
+ */
+export function getFullBuild(
+  token: string,
+  buildNumber: number,
+  vcs: GitInfo
+): Promise<FetchBuildResponse> {
+  const url = `${createVcsUrl(vcs)}/${buildNumber}`;
+  return client(token).get<FetchBuildResponse>(url);
+}
+
+/**
+ * Get the latest build artifacts for a project
+ *
  * @see https://circleci.com/docs/api/v1-reference/#build-artifacts-latest
+ * @example GET - /project/:vcs-type/:username/:project/latest/artifacts?branch=:branch&filter=:filter
  */
 export function getLatestArtifacts(
   token: string,
@@ -136,7 +205,7 @@ export function circleci({
       return validateRequest(postFollowNewProject, opts);
     },
 
-    recentBuilds: (opts?: Options) => getRecentBuilds(token),
+    recentBuilds: (opts?: Options) => getRecentBuilds(token, opts),
 
     latestArtifacts: (opts?: CircleRequest): Promise<ArtifactResponse> => {
       return validateRequest(getLatestArtifacts, opts);
